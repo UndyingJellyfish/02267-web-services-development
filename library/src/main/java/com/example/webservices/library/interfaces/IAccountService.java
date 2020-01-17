7d6a45e2d2201f9c635b1e20cfe33258cf4c4bc3
Emil Damsbo <s164395@student.dtu.dk>
2020-01-17 16:20:30 +0100

    add accountservice

--- library/src/main/java/com/example/webservices/library/interfaces/IAccountService.java
+++ library/src/main/java/com/example/webservices/library/interfaces/IAccountService.java
@@ -1,14 +1,24 @@
 package com.example.webservices.library.interfaces;
 
 import com.example.webservices.library.dataTransferObjects.AccountDto;
+import com.example.webservices.library.dataTransferObjects.SignupDto;
+import com.example.webservices.library.exceptions.DuplicateEntryException;
 import com.example.webservices.library.exceptions.EntryNotFoundException;
 
 import java.util.UUID;
 
 public interface IAccountService {
+    AccountDto addCustomer(SignupDto signupDto) throws DuplicateEntryException;
+
+    AccountDto addMerchant(SignupDto signupDto) throws DuplicateEntryException;
+
+    void changeName(UUID accountId, String newName) throws EntryNotFoundException;
+
+    void delete(UUID accountId) throws EntryNotFoundException;
+
     AccountDto getCustomer(UUID customerId) throws EntryNotFoundException;
 
     AccountDto getAccount(UUID id) throws EntryNotFoundException;
 
-    AccountDto getMerchant(UUID merchantId)throws EntryNotFoundException;
+    AccountDto getMerchant(UUID merchantId) throws EntryNotFoundException;
 }
