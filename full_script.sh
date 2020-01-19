9d5f54933db745ca217cefc90073c47abcf9aba6
Alexander <s164424@student.dtu.dk>
2020-01-19 11:11:14 +0100

    Small Changes

--- full_script.sh
+++ full_script.sh
@@ -10,13 +10,9 @@ bash build_and_test.sh tokens
 bash build_and_test.sh accounts
 bash build_and_test.sh transactions
 
-if [ $1 ]
-then
-  bash systemTests.sh application
-  docker-compose -f docker-compose.yml up -d
-else
-  docker-compose -f docker-compose.yml up -d --scale application=0 --scale accounts=0
-fi
+bash systemTests.sh application
+docker-compose -f docker-compose.yml up -d
+
 
 
 
