7d2d1fed4dd063120b9b9f961458be7b7f74b6d9
Alexander <s164424@student.dtu.dk>
2020-01-20 11:43:27 +0100

    Trying with Gson

--- accounts/src/main/java/com/example/webservices/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/example/webservices/accounts/AccountsApplication.java
@@ -1,5 +1,6 @@
 package com.example.webservices.accounts;
 
+import com.google.gson.*;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
@@ -8,12 +9,50 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.Json;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.lang.reflect.Type;
 
 import static com.example.webservices.library.RabbitHelper.*;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {JacksonAutoConfiguration.class})
 public class AccountsApplication {
+
+    @Bean
+    public Docket myApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    public static class SpringfoxJsonToGsonAdapter implements JsonSerializer<Json> {
+
+        @Override
+        public JsonElement serialize(Json json, Type type, JsonSerializationContext context) {
+            final JsonParser parser = new JsonParser();
+            return parser.parse(json.value());
+        }
+    }
+
+    @Bean
+    public GsonHttpMessageConverter gsonHttpMessageConverter() {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+        converter.setGson(gson());
+        return converter;
+    }
+    private Gson gson() {
+        final GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(Json.class, new SpringfoxJsonToGsonAdapter());
+        return builder.create();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
