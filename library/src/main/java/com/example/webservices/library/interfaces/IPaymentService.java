615b950aa5b087b14e7177948e7acdef1548959f
Emil Damsbo <s164395@student.dtu.dk>
2020-01-20 19:12:43 +0100

    Add javadoc comments to all service interfaces

--- library/src/main/java/com/example/webservices/library/interfaces/IPaymentService.java
+++ library/src/main/java/com/example/webservices/library/interfaces/IPaymentService.java
@@ -6,7 +6,21 @@ import com.example.webservices.library.exceptions.*;
 import java.util.UUID;
 
 public interface IPaymentService {
+    /**
+     * @param transactionDto data object used to transfer money from one account to another using a token
+     * @return gives the transfer details
+     * @throws EntryNotFoundException thrown when either of the accounts (debtor or creditor) do not exist
+     * @throws TokenException thrown when the token specified in {@param transactionDto} is invalid
+     * @throws BankException thrown when the bank is unavailable for transfers at the current time
+     * @throws InvalidTransferAmountException thrown when the amount of money specified is not allowed
+     * @throws DuplicateEntryException thrown when the transaction already exists
+     */
     TransactionDto transfer(TransactionDto transactionDto) throws EntryNotFoundException, TokenException, BankException, InvalidTransferAmountException, DuplicateEntryException;
 
+    /**
+     * @param transactionId the transaction to refund
+     * @throws EntryNotFoundException thrown when the transaction does not exist
+     * @throws DuplicateEntryException thrown when the transaction has already been refunded
+     */
     void refund(UUID transactionId) throws EntryNotFoundException, DuplicateEntryException;
 }
