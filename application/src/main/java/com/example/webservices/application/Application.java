bd5308a2534f26114f6751c1c3f8c6f6fded9c89
Alexander <s164424@student.dtu.dk>
2020-01-20 11:18:10 +0100

    Trying with Gson

--- application/src/main/java/com/example/webservices/application/Application.java
+++ application/src/main/java/com/example/webservices/application/Application.java
@@ -1,16 +1,37 @@
 package com.example.webservices.application;
 
+import com.google.gson.Gson;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication(scanBasePackages = "com.example.webservices")
+@ConditionalOnClass(Gson.class)
+@ConditionalOnMissingClass(value = "com.fasterxml.jackson.core.JsonGenerator")
+@ConditionalOnBean(Gson.class)
 public class Application {
+
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public GsonHttpMessageConverter gsonHttpMessageConverter(Gson gson) {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+        converter.setGson(gson);
+        return converter;
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
