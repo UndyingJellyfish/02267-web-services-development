6b42c74ca7414276ef431cf6ad805557607d9073
Alexander <s164424@student.dtu.dk>
2020-01-20 11:36:50 +0100

    Trying with Gson

--- application/src/main/java/com/example/webservices/application/Application.java
+++ application/src/main/java/com/example/webservices/application/Application.java
@@ -1,37 +1,59 @@
 package com.example.webservices.application;
 
-import com.google.gson.Gson;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import com.google.gson.*;
 import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.http.converter.json.GsonHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.async.DeferredResult;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.json.Json;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(scanBasePackages = "com.example.webservices")
-@ConditionalOnClass(Gson.class)
-@ConditionalOnMissingClass(value = "com.fasterxml.jackson.core.JsonGenerator")
-@ConditionalOnBean(Gson.class)
+import java.lang.reflect.Type;
+
+@SpringBootApplication(scanBasePackages = "com.example.webservices",exclude = {JacksonAutoConfiguration.class})
+@EnableSwagger2
 public class Application {
 
-
-
     @Bean
-    @ConditionalOnMissingBean
-    public GsonHttpMessageConverter gsonHttpMessageConverter(Gson gson) {
-        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
-        converter.setGson(gson);
-        return converter;
+    public Docket myApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+    public static class SpringfoxJsonToGsonAdapter implements JsonSerializer<Json> {
+
+        @Override
+        public JsonElement serialize(Json json, Type type, JsonSerializationContext context) {
+            final JsonParser parser = new JsonParser();
+            return parser.parse(json.value());
+        }
     }
 
+    @Bean
+    public GsonHttpMessageConverter gsonHttpMessageConverter() {
+        GsonHttpMessageConverter converter = new GsonHttpMessageConverter();
+        converter.setGson(gson());
+        return converter;
+    }
+    private Gson gson() {
+        final GsonBuilder builder = new GsonBuilder();
+        builder.registerTypeAdapter(Json.class, new SpringfoxJsonToGsonAdapter());
+        return builder.create();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
