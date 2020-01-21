f4f950d312e997d53722212ac2534181bbb21a4c
Emil Damsbo <s164395@student.dtu.dk>
2020-01-21 15:13:37 +0100

    fix more javadocs

--- library/src/main/java/com/example/webservices/library/RabbitMQBaseClass.java
+++ library/src/main/java/com/example/webservices/library/RabbitMQBaseClass.java
@@ -27,12 +27,22 @@ public abstract class RabbitMQBaseClass extends RabbitHelper {
 
     /**
      * @author s164424
+     * @param <T> type of message to publish
+     * @param queue which queue to push the message to
+     * @param message which message to send
+     * @return the deserialized instance of type {@link T}
      */
     protected <T> ResponseObject send(String queue, T message){
         return send(queue, message, ResponseObject.class);
     }
     /**
      * @author s164424
+     * @param <T> type of the published message
+     * @param <T2> type of the return time
+     * @param queue which queue to push the message to
+     * @param message which message to send
+     * @param type given type to deserialize into
+     * @return the deserialized instance of type {@link T2}
      */
     protected <T, T2> T2 send(String queue, T message, Class<T2> type){
         return (T2) rabbitTemplate.convertSendAndReceive(exchange.getName(),
